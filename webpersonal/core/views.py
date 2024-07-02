54975c982533a46d5bdcfff928f5fc084528462a
guillesese <guillermosese@hotmail.es>
2024-07-02 08:42:37 +0200

    commit

---
+++ webpersonal/core/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, HttpResponse
+
+#HttpResponse permite contestar una peticion devolviendo un codigo. 
+#  
+
+html_base = """
+<h1>Mi web personal</h1>
+<ul> 
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about-me/">Acerca de</a></li>
+    <li><a href="/portfolio/">Portfolio</a></li>
+    <li><a href="/contact/">Contacto</a></li>
+</ul>
+"""
+
+'''Metodo de la portada
+    request: peticion de que se quiere ver la portada. '''
+def home (request):
+    # se devuelve usando la plantilla templates\core\home.html
+    return render(request,"core/home.html")
+
+def about(request):
+    return render(request,"core/about.html")
+
+def portfolio(request):
+    return render(request,"core/portfolio.html")
+
+def contact(request):
+    return render(request,"core/contact.html")
